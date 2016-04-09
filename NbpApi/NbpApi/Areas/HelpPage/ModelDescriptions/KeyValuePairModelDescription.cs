117a8477bdf7bf79fc33be75ebedfb45c9cc19cb
lukaszloba <lukaszloba@gmail.com>
2016-04-09 14:58:10 +0200

    clean

--- NbpApi/NbpApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace NbpApi.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
